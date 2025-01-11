a45062d671deff212269a8b52a33283f6c46f90f
Moaz Mirza <moazmirza13@gmail.com>
2025-01-11 05:15:15 +0500

    scoring

--- scenes/level.gd
+++ scenes/level.gd
@@ -6,3 +6,12 @@ func _on_rock_timer_timeout():
 	var rock = rock_scene.instantiate()
 	
 	$Rocks.add_child(rock)
+	
+# scoring 
+var score = 0
+
+func _process(delta):
+	# Update the survival time
+	score += 1
+	var score_lable = $UI/Score
+	score_lable.text = str(score) + "m"
