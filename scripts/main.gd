0e1a65fbfc39a1ed058b5732bfb1b443671862df
RehmahMuneeb <rehmahbaber47@gmail.com>
2025-03-19 03:37:06 +0500

    label

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,19 +4,31 @@ extends Control
 @onready var capacity_button = $"infopanel/Bucket Capacity/Button"
 @onready var well_button = $infopanel/Button  # Well depth upgrade button
 
+# New Labels for Bucket Capacity & Well Depth
+@onready var bucket_capacity_label = $CapacityLabel
+@onready var well_depth_label = $DepthLabel
+
 func _ready() -> void:
 	update_ui()
 	capacity_button.pressed.connect(upgrade_capacity)
-	well_button.pressed.connect(upgrade_well_depth)  # Connect button
+	well_button.pressed.connect(upgrade_well_depth)
 
 func _process(_delta):
 	update_ui()  # Refresh UI
 
 func update_ui():
+	# Update Coins Display
 	coin_label.text = "Coins: " + str(Global.score)  
+
+	# Update Button Text
 	capacity_button.text = str(Global.bucket_upgrade_cost) + "\nUpgrade"
 	well_button.text = str(Global.well_upgrade_cost) + "\nUpgrade"
 
+	# Update Labels for Bucket Capacity & Well Depth
+	bucket_capacity_label.text = "Capacity: " + str(Global.bucket_capacity)
+	well_depth_label.text = "Depth: " + str(Global.well_depth_limit)
+
+	# Disable Buttons if Not Enough Coins
 	capacity_button.disabled = Global.score < Global.bucket_upgrade_cost
 	well_button.disabled = Global.score < Global.well_upgrade_cost
 
@@ -24,7 +36,7 @@ func upgrade_capacity():
 	if Global.score >= Global.bucket_upgrade_cost:
 		Global.score -= Global.bucket_upgrade_cost  
 		Global.bucket_capacity += 1  # Increase by 1  
-		Global.bucket_upgrade_cost *= 2  # Multiply upgrade cost by 3  
+		Global.bucket_upgrade_cost *= 2  # Multiply upgrade cost by 2  
 		Global.save_game()
 		update_ui()
 
@@ -32,7 +44,7 @@ func upgrade_well_depth():
 	if Global.score >= Global.well_upgrade_cost:
 		Global.score -= Global.well_upgrade_cost  
 		Global.well_depth_limit += 500  # Increase depth limit by 500  
-		Global.well_upgrade_cost *= 2  # Multiply upgrade cost by 3  
+		Global.well_upgrade_cost *= 2  # Multiply upgrade cost by 2  
 		Global.save_game()
 		update_ui()
 
