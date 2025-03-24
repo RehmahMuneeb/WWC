2a92ae89761befc0eace91b1d234a344420bb700
RehmahMuneeb <rehmahbaber47@gmail.com>
2025-03-24 22:20:08 +0500

    slots

--- scripts/main.gd
+++ scripts/main.gd
@@ -50,3 +50,7 @@ func upgrade_well_depth():
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://scenes/level.tscn")
+
+
+func _on_rare_items_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/RareItems.tscn")
