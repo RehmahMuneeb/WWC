a66456c9a24b49dccacf266f00355f30f1846f99
RehmahMuneeb <rehmahbaber47@gmail.com>
2025-04-07 15:39:19 +0500

    sell

--- scenes/main_inventory.gd
+++ scenes/main_inventory.gd
@@ -1,24 +1,28 @@
 extends Control
 
-var selected_gem = null
+@onready var gem_inventory = $Tabs/GemsTab/Inventory
+@onready var sell_button = $SellButton  # Your button
+@onready var coin_label = $Coins/Coinslabel
+
+var selected_texture: Texture = null
 
 func _ready():
-	# Connect gem selection signal
-	$Tabs/GemsTab/Inventory.connect("gem_selected", Callable(self, "_on_gem_selected"))
-	$SellButton.disabled = true
+	gem_inventory.connect("gem_clicked", Callable(self, "_on_gem_clicked"))
+	sell_button.pressed.connect(_on_sell_pressed)
+	update_coin_label()
 
-func _on_gem_selected(gem_node):
-	selected_gem = gem_node
-	$SellButton.disabled = false
+func _on_gem_clicked(texture: Texture):
+	selected_texture = texture
+	print("Selected texture to sell:", texture)
 
-func _on_SellButton_pressed():
-	if selected_gem:
-		var texture = selected_gem.get_node("TextureRect").texture
-		Global.collected_gems.erase(texture)
-		selected_gem.queue_free()
-		selected_gem = null
-		$SellButton.disabled = true
+func _on_sell_pressed():
+	if selected_texture and Global.collected_gems.has(selected_texture):
+		Global.collected_gems.erase(selected_texture)
+		Global.score += 10  # Sell value per gem
+		gem_inventory.update_inventory()
+		update_coin_label()
+		selected_texture = null
 
-
-func _on_sell_button_pressed() -> void:
-	pass # Replace with function body.
+func update_coin_label():
+	if coin_label:
+		coin_label.text = "COINS: " + str(Global.score)  # Access score as coins
