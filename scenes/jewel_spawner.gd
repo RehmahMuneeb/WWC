487829eab692713189951a3bee039b006e048571
RehmahMuneeb <rehmahbaber47@gmail.com>
2025-04-10 02:21:01 +0500

    speed

--- scenes/jewel_spawner.gd
+++ scenes/jewel_spawner.gd
@@ -3,32 +3,45 @@ extends Node2D
 # Preload the jewel scene
 @export var jewel_scene: PackedScene
 
-# Spawn interval in seconds
-@export var spawn_interval: float = 1.0
+# Spawn interval range (in seconds)
+@export var min_spawn_interval: float = 1.0
+@export var max_spawn_interval: float = 2.0
 
 # Maximum number of jewels allowed on screen
 @export var max_jewels: int = 12
 
 func _ready() -> void:
-	# Start the spawn timer
-	$SpawnTimer.wait_time = spawn_interval
+	randomize()
+	start_spawn_timer()
+
+func start_spawn_timer():
+	# Set a random wait time between min and max
+	var random_interval = randf_range(min_spawn_interval, max_spawn_interval)
+	$SpawnTimer.wait_time = random_interval
 	$SpawnTimer.start()
 
 func _on_spawn_timer_timeout() -> void:
-	# Check how many jewels are currently in the scene
 	var current_jewels = get_tree().get_nodes_in_group("jewel").size()
+	var remaining = max_jewels - current_jewels
 
-	# Spawn a new jewel if the maximum number hasn't been reached
-	if current_jewels < max_jewels:
+	if remaining > 0:
 		spawn_jewel()
+
+		# 30% chance to spawn a second jewel shortly after
+		if remaining > 1 and randf() < 0.3:
+			var delay = randf_range(0.5, 0.8)
+			await get_tree().create_timer(delay).timeout
+			spawn_jewel()
+
+	start_spawn_timer()
 
 func spawn_jewel() -> void:
 	var jewel = jewel_scene.instantiate()
-	
-	# Set the initial position of the jewel
+
+	# Set random initial position
 	var viewport_width = get_viewport_rect().size.x
-	var spawn_x = randf_range(50, viewport_width - 50)  # Random x position within the screen width
-	var spawn_y = -50  # Spawn above the top edge of the screen
+	var spawn_x = randf_range(50, viewport_width - 50)
+	var spawn_y = -50  # Just above the screen
 	jewel.position = Vector2(spawn_x, spawn_y)
-	
+
 	add_child(jewel)
