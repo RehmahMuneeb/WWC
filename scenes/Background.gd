d35a5f485ba6396db4b26e49abe3be1a8e674657
RehmahMuneeb <rehmahbaber47@gmail.com>
2025-04-30 18:55:09 +0500

    transition

--- scenes/Background.gd
+++ scenes/Background.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 @onready var air_effect = $AirEffect
 @onready var normal_bg = $WallImg  # Normal background
-@onready var lava_bg = $WallImg2    # Single lava background for all danger zones
-@onready var ice_bg = $WallImg5      # Ice background
+@onready var lava_bg = $WallImg2   # Lava background for all danger zones
+@onready var ice_bg = $WallImg5    # Ice background
 @onready var warning_label = $WarningLabel
 @onready var jewel_spawner = $"../JewelSpawner"
 
@@ -13,7 +13,7 @@ var warning_shown = false
 var last_warning_zone = -1
 
 func _ready():
-	main_script = get_parent()  # Main script is parent node
+	main_script = get_parent()
 	air_effect.visible = false
 	air_effect.emitting = false
 	normal_bg.visible = true
@@ -23,15 +23,12 @@ func _ready():
 	warning_label.modulate.a = 0.0
 
 func _process(delta):
-	score = main_script.score  # Sync score with main script
-	var depth = score % 12000
+	score = main_script.score
+	var depth = score % 11000
 	
-	# Check for upcoming danger zones (100m before)
 	_check_upcoming_danger_zones(depth)
-	
-	# Check current zone and update background
 	_update_background_based_on_zone(depth)
-	
+
 	# Air effect: 2000-2999
 	if depth >= 2000 and depth < 3000:
 		if not air_effect.emitting:
@@ -43,35 +40,33 @@ func _process(delta):
 			air_effect.emitting = false
 
 func _check_upcoming_danger_zones(depth: int):
-	# Check all zones for upcoming danger (100m before)
 	for i in range(main_script.zone_depths.size()):
 		var zone_start = main_script.zone_depths[i]
 		var warning_start = zone_start - 100
-		
-		# Check if we're in the warning zone but haven't shown the warning yet
+
 		if depth >= warning_start and depth < zone_start and last_warning_zone != i:
 			show_warning("DANGER\nAHEAD!")
 			last_warning_zone = i
 			break
-		# Reset warning tracking when we pass the zone
-		elif depth >= zone_start and last_warning_zone == i:
+		elif depth >= zone_start + main_script.zone_width and last_warning_zone == i:
 			last_warning_zone = -1
 
 func _update_background_based_on_zone(depth: int):
 	var in_danger_zone = false
 	
-	# Check if we're in any danger zone
 	for i in range(main_script.zone_depths.size()):
 		if depth >= main_script.zone_depths[i] and depth < main_script.zone_depths[i] + main_script.zone_width:
 			in_danger_zone = true
 			break
-	
-	# Ice zone (special case)
-	if depth >= 10000 and depth < 11000:
+
+	# Ice zone now recurs every 11000m, from 10000 to 10999
+	var in_ice_zone = depth >= 10000 and depth < 11000
+
+	if in_ice_zone:
 		_set_background_zone(2)  # Ice
 		jewel_spawner.set_lava_active(false)
 	elif in_danger_zone:
-		_set_background_zone(1)  # Lava (all danger zones use same background)
+		_set_background_zone(1)  # Lava
 		jewel_spawner.set_lava_active(true)
 	else:
 		_set_background_zone(0)  # Normal
--- scripts/level.gd
+++ scripts/level.gd
@@ -6,6 +6,7 @@ var lava2_texture = preload("res://assets/Lavarock88.png")
 var lava3_texture = preload("res://assets/Lava-stone-33.png")
 
 var score = 0
+var last_cycle = -1
 
 var zone_depths = []
 var zone_types = []
@@ -23,6 +24,7 @@ var safe_start_area = 2000
 
 func _ready():
 	score = 0
+	last_cycle = -1
 	depth_bar.min_value = 0
 	depth_bar.max_value = 10000
 	rock_timer.wait_time = 2.0
@@ -42,7 +44,7 @@ func randomize_zones():
 	types.shuffle()
 	zone_types = types.duplicate()
 
-	var available_space = 11000 - safe_start_area - (total_zones * zone_width) - ((total_zones - 1) * min_zone_gap)
+	var available_space = 10000 - safe_start_area - (total_zones * zone_width) - ((total_zones - 1) * min_zone_gap)
 
 	var positions = []
 	for i in range(total_zones - 1):
@@ -56,15 +58,22 @@ func randomize_zones():
 			start_pos = positions[i-1] + zone_width + min_zone_gap + safe_start_area
 
 		var segment_start = last_pos
-		var segment_end = positions[i] + safe_start_area if i < positions.size() else 12000
+		var segment_end = positions[i] + safe_start_area if i < positions.size() else 10000
 		var random_offset = randi_range(0, max(0, segment_end - segment_start - zone_width))
 
 		zone_depths.append(segment_start + random_offset)
 		last_pos = zone_depths[i] + zone_width + min_zone_gap
 
+	print("New zones randomized at depths: ", zone_depths, " with types: ", zone_types)
+
 func _process(delta):
 	score += 1
 	score_label.text = str(score) + "m"
+
+	var current_cycle = score / 11000
+	if current_cycle != last_cycle:
+		randomize_zones()
+		last_cycle = current_cycle
 
 	var cycle_depth = score % 11000
 	if cycle_depth < 10000:
@@ -77,14 +86,14 @@ func _process(delta):
 
 func update_rock_spawn_speed(depth: int):
 	var spawn_rate = 3.5
-
-	var in_ice_zone = (depth % 11000) >= 10000
+	var cycle_depth = depth % 11000
+	var in_ice_zone = cycle_depth >= 10000
 
 	if in_ice_zone:
 		spawn_rate = 1000.0  # Effectively disables rock spawn
 	else:
 		for i in range(zone_depths.size()):
-			if depth >= zone_depths[i] and depth < zone_depths[i] + zone_width:
+			if cycle_depth >= zone_depths[i] and cycle_depth < zone_depths[i] + zone_width:
 				match zone_types[i]:
 					0: spawn_rate = 1.0
 					1: spawn_rate = 0.6
@@ -96,11 +105,13 @@ func update_rock_spawn_speed(depth: int):
 func set_rock_spawn_rate(rate: float):
 	if rock_timer.wait_time != rate:
 		rock_timer.wait_time = rate
-		rock_timer.start()
+		if rate < 1000:  # Only restart if not in ice zone
+			rock_timer.start()
 
 func _on_rock_timer_timeout():
 	var depth = score
-	var in_ice_zone = (depth % 11000) >= 10000
+	var cycle_depth = depth % 11000
+	var in_ice_zone = cycle_depth >= 10000
 
 	if in_ice_zone:
 		return  # Do not spawn rocks during Ice Zone
@@ -111,7 +122,7 @@ func _on_rock_timer_timeout():
 
 	var current_zone_type = -1
 	for i in range(zone_depths.size()):
-		if depth >= zone_depths[i] and depth < zone_depths[i] + zone_width:
+		if cycle_depth >= zone_depths[i] and cycle_depth < zone_depths[i] + zone_width:
 			current_zone_type = zone_types[i]
 			break
 
@@ -148,7 +159,8 @@ func _on_rock_timer_timeout():
 	$Rocks.add_child(rock)
 
 func update_jewel_spawn(depth: int):
-	var in_ice_zone = (depth % 11000) >= 10000
+	var cycle_depth = depth % 11000
+	var in_ice_zone = cycle_depth >= 10000
 
 	if in_ice_zone:
 		jewel_spawner.min_spawn_interval = 0.1
