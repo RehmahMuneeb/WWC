4d1b92850a219f8db16087a35341940d8fcbc69b
RehmahMuneeb <rehmahbaber47@gmail.com>
2025-07-04 09:45:21 +0500

    button pressed

--- chest_scene.gd
+++ chest_scene.gd
@@ -9,17 +9,17 @@ var jewel_data = {
 	}
 }
 
-var player_coins = 100
+
 
 # Node references
 @onready var jewel_node = $ScrollContainer/GridContainer/CrabJewel
 @onready var jewel_image = jewel_node.get_node("JewelImage")
 @onready var progress_label = jewel_node.get_node("Label")
 @onready var add_button = jewel_node.get_node("Button")
-
+@onready var coin_label = $CoinLabel
 func _ready():
 	update_jewel("CrabJewel")
-
+	update_coin_display()
 # Called whenever we need to refresh the UI
 func update_jewel(jewel_id: String):
 	var data = jewel_data[jewel_id]
@@ -33,14 +33,17 @@ func update_jewel(jewel_id: String):
 
 	# Disable Add button if already unlocked
 	add_button.disabled = data.unlocked
-
+	
+func update_coin_display():
+	if coin_label:
+		coin_label.text = "COINS: " + str(Global.score)
 # Called when Add Coins button is pressed
-func _on_AddCoins_pressed():
+func _on_button_pressed():
 	var id = "CrabJewel"
 	var data = jewel_data[id]
 
-	if player_coins >= 10 and not data.unlocked:
-		player_coins -= 10
+	if Global.score >= 10 and not data.unlocked:
+		Global.score -= 10  # Deduct from real coin pool
 		data.progress += 10
 
 		if data.progress >= data.max:
@@ -48,3 +51,4 @@ func _on_AddCoins_pressed():
 			data.unlocked = true
 
 		update_jewel(id)
+		update_coin_display()  # Update visible coin count
