1d5721750c6c43301b93f21279ae5d7729a29df4
RehmahMuneeb <rehmahbaber47@gmail.com>
2025-07-10 20:59:38 +0500

    speed bug

--- scenes/wall_img.gd
+++ scenes/wall_img.gd
@@ -1,11 +1,16 @@
 extends Sprite2D
 
-var target_speed = 0.9
-var min_speed = 0.3
-var scroll_speed = 0.3
-var speed_increment = 0.001  # Adjust for how fast you want it to increase
+var scroll_offset := 0.0
+var target_speed := 0.9
+var min_speed := 0.3
+var scroll_speed := 0.3
+var speed_increment := 0.001  # Adjust how fast it accelerates
+
+@onready var mat := material
 
 func _process(delta):
 	if scroll_speed < target_speed:
 		scroll_speed = min(scroll_speed + speed_increment * delta, target_speed)
-		material.set_shader_parameter("scroll_speed", scroll_speed)
+
+	scroll_offset += scroll_speed * delta
+	mat.set_shader_parameter("scroll_offset", scroll_offset)
